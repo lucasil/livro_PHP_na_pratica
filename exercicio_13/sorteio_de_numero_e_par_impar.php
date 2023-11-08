5de26d1fff814760f84f1199787f6ce14074f79f
Lucas Silveira <lucas07.macedo@gmail.com>
2023-11-08 17:12:19 -0300

    Primeiro commit

--- exercicio_13/sorteio_de_numero_e_par_impar.php
+++ exercicio_13/sorteio_de_numero_e_par_impar.php
@@ -10,7 +10,13 @@
     <h1>Exercício 13 - Sorteio de Número e par/ímpar</h1>
 
     <?php
-    
+        $num = rand(1 , 10);
+
+        if ($num % 2 == 0) {
+            echo "O número $num é par!";
+        } else {
+            echo "O número $num é ímpar!";
+        }
     ?>
 </body>
 </html>
