b386fc3b79d4338a48002eb9f81f8f9daea526be
Lucas Silveira <lucas07.macedo@gmail.com>
2023-11-08 16:54:49 -0300

    Primeiro commit

---
+++ exercicio_11/maior_idade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Maior Idade</title>
+</head>
+<body>
+    <h1>Exercício 11 - Maior Idade</h1>
+
+    <?php
+        $amalia = 17;
+        $joaquim = 18;
+
+        if ($amalia > $joaquim) {
+            echo "A pessoa mais velha é Amalia.";
+        } else {
+            echo "A pessoa mais velha é Joaquim.";
+        }
+    ?>
+</body>
+</html>
---
+++ exercicio_12/saudacao_conforme_o_horario.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Saudação Conforme o Horário</title>
+</head>
+<body>
+    <h1>Exercício 12 - Saudação Conforme o Horário</h1>
+
+    <?php
+        date_default_timezone_set("America/Sao_Paulo"); // define o fuso horário
+        
+        $agora = date("H"); // a função date() define a data e hora afinal, dependendo do comando que é inserido dentro dela
+
+        if ($agora < 12) {
+            echo "<p style='color:green'>Bom dia!</p>";        
+        } elseif ($agora <= 18) {
+            echo "<p style='color:red'>Boa tarde!</p>";        
+        } else {
+            echo "<p style='color:blue'>Boa noite!</p>";        
+        }
+    ?>
+    
+</body>
+</html>
