7e0a84e02241b73f6f48678639cd2dd3f3eda305
Lucas Silveira <lucas07.macedo@gmail.com>
2023-11-08 16:58:42 -0300

    Primeiro commit

---
+++ exercicio_13/sorteio_de_numero_e_par_impar.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sorteio de Número e par/ímpar</title>
+</head>
+<body>
+    <h1>Exercício 13 - Sorteio de Número e par/ímpar</h1>
+
+    <?php
+    
+    ?>
+</body>
+</html>
